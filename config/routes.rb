cd488fccef73af7b1b086f191032ec839932fceb
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-04-22 11:40:33 +0900

    自動生成ファイルはrubocopの利用をしないようにymlファイルに記述, rubocop実施によるファイルの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cooking_repertoires, only:[:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :cooking_repertoires, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   root to: 'cooking_repertoires#index'
 end
