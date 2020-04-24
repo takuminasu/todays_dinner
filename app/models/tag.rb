c13a271f550899fade42671ef8e32c2ef952284b
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-04-24 15:04:19 +0900

    eraseをdeleteに記述変更

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
   has_many :cooking_repertoire_tags
   has_many :cooking_repertoires, through: :cooking_repertoire_tags
 
-  scope :valid, -> { where.not(name: I18n.t('activerecord.attributes.tag.erase')) }
+  scope :valid, -> { where.not(name: I18n.t('activerecord.attributes.tag.delete')) }
 end
