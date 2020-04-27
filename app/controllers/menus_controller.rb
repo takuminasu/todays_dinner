3a4d4d6c5c1ff3a3ba08e2586c496a5f8f403e65
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-04-27 17:35:39 +0900

    ランダムに1週間決める機能のみ実装

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,16 +1,21 @@
 class MenusController < ApplicationController
+  def index
+    @menus = Menu.all
+  end
   def new
     @menu = Menu.new
   end
 
   def create
-    @menu = Menu.new(menu_params.merge(cooking_repertoire_id: CookingRepertoire.random_id))
-
-    if @menu.save
-      redirect_to menu_path(@menu)
-    else
-      render :new
+    7.times do |i|
+      @menu = Menu.new(menu_params.merge(cooking_repertoire_id: CookingRepertoire.random_id))
+      puts i
+      if 0 < i
+        @menu.date += i
+      end
+      @menu.save
     end
+    redirect_to menus_path
   end
 
   def show
