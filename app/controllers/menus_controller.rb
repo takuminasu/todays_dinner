bd514ac99f76aaf4e181c2ebe7bd3796b4ed21b7
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-04-28 17:05:37 +0900

    find_or_initialize_byに記述変更, アクション内のcase文の内容をメソッドに分割

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,26 +2,17 @@ class MenusController < ApplicationController
   def index
     @menus = Menu.all
   end
+
   def new
     @menu = Menu.new
   end
 
   def create
-    case params["menu"]["period_id"]
-    when "1" then
-      @menu = Menu.new(menu_params.merge(cooking_repertoire_id: CookingRepertoire.random_id))
-      if @menu.save
-        redirect_to menu_path(@menu)
-      else
-        render :new
-      end
-    when "2" then
-      7.times do |i|
-        @menu = Menu.new(menu_params.merge(cooking_repertoire_id: CookingRepertoire.random_id))
-        @menu.date += i
-        @menu.save
-      end
-      redirect_to menus_path
+    case params['menu']['period_id']
+    when '1'
+      one_day
+    when '2'
+      one_week
     end
   end
 
@@ -33,5 +24,20 @@ class MenusController < ApplicationController
 
   def menu_params
     params.require(:menu).permit(:date)
+  end
+
+  def one_day
+    menu = Menu.find_or_initialize_by(menu_params)
+    menu.update_attributes(menu_params.merge(cooking_repertoire_id: CookingRepertoire.random_id))
+    redirect_to menu_path(menu)
+  end
+
+  def one_week
+    7.times do |i|
+      date = (Date.parse(params['menu']['date']) + i).strftime
+      menu = Menu.find_or_initialize_by(date: date)
+      menu.update_attributes({ date: date }.merge(cooking_repertoire_id: CookingRepertoire.random_id))
+    end
+    redirect_to menus_path
   end
 end
