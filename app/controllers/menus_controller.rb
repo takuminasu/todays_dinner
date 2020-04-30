9b5d2cf05a75f6133c69df69fe94350d19e18007
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-04-30 17:09:18 +0900

    翻訳対応, ロジックをモデルに持たせて処理を共通化

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,6 +1,6 @@
 class MenusController < ApplicationController
   def index
-    @menus = Menu.all
+    @menus = Menu.where('date >= ?', Date.today)
   end
 
   def new
@@ -8,36 +8,9 @@ class MenusController < ApplicationController
   end
 
   def create
-    case params['menu']['period_id']
-    when '1'
-      one_day
-    when '2'
-      one_week
-    end
-  end
-
-  def show
-    @menu = Menu.find_by(date: Date.today)
-  end
-
-  private
-
-  def menu_params
-    params.require(:menu).permit(:date)
-  end
-
-  def one_day
-    menu = Menu.find_or_initialize_by(menu_params)
-    menu.update_attributes(menu_params.merge(cooking_repertoire_id: CookingRepertoire.random_id))
-    redirect_to menu_path(menu)
-  end
-
-  def one_week
-    7.times do |i|
-      date = (Date.parse(params['menu']['date']) + i).strftime
-      menu = Menu.find_or_initialize_by(date: date)
-      menu.update_attributes({ date: date }.merge(cooking_repertoire_id: CookingRepertoire.random_id))
-    end
+    period = params[:menu][:period_id].to_i
+    date = Date.parse(params[:menu][:date])
+    Menu.select_days(period, date)
     redirect_to menus_path
   end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,4 +2,12 @@ class Menu < ApplicationRecord
   validates :date, presence: true, uniqueness: true
 
   belongs_to :cooking_repertoire
+
+  def self.select_days(period, date)
+    period.times do
+      menu = Menu.find_or_initialize_by(date: date)
+      menu.update_attributes({ date: date }.merge(cooking_repertoire_id: CookingRepertoire.random_id))
+      date += 1
+    end
+  end
 end
