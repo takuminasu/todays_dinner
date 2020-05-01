c3a2e9b360a7593d0fa13dfe3f656c26af127868
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-05-01 19:00:02 +0900

    options_for_selectの利用による記述の変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def periods
+    { I18n.t('.menus.new.one_day') => 1, I18n.t('.menus.new.seven_days') => 7 }
+  end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,4 @@
 class Menu < ApplicationRecord
-  enum period: { I18n.t('activerecord.attributes.menu.period.one_day') => 1,
-                 I18n.t('activerecord.attributes.menu.period.seven_days') => 7 }
   validates :date, presence: true, uniqueness: true
 
   belongs_to :cooking_repertoire
@@ -8,10 +6,8 @@ class Menu < ApplicationRecord
   def self.create_for(period, date)
     to = date + period - 1
     (date..to).each do |day|
-      transaction do
-        menu = Menu.find_or_initialize_by(date: day)
-        menu.update_attributes!({ date: day, cooking_repertoire_id: CookingRepertoire.random_id })
-      end
+      menu = Menu.find_or_initialize_by(date: day)
+      menu.update_attributes!({ date: day, cooking_repertoire_id: CookingRepertoire.random_id })
     end
   end
 end
