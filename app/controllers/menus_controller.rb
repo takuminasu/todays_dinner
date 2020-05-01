282d2f1975848528b73d38ec808cfadbaad361fc
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-05-01 20:00:41 +0900

    create内のredirectの処理のprivate以下に記述

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -9,12 +9,19 @@ class MenusController < ApplicationController
 
   def create
     period = params[:menu][:period].to_i
-    date = Date.parse(params[:menu][:date])
-    Menu.create_for(period, date)
-    if params[:menu][:period] == '1'
-      redirect_to menus_path, notice: t('.added_menu', { day: date })
+    from_date = Date.parse(params[:menu][:date])
+    to_date = from_date + period - 1
+    Menu.make(from_date, to_date)
+    redirect_to menus_path, notice: added_message(from_date, to_date)
+  end
+
+  private
+
+  def added_message(from_date, to_date)
+    if from_date == to_date
+      t('.added_menu', { day: from_date })
     else
-      redirect_to menus_path, notice: t('.added_menus', { start_day: date, end_day: date + period })
+      t('.added_menus', { start_day: from_date, end_day: to_date })
     end
   end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,9 +3,8 @@ class Menu < ApplicationRecord
 
   belongs_to :cooking_repertoire
 
-  def self.create_for(period, date)
-    to = date + period - 1
-    (date..to).each do |day|
+  def self.make(from, to)
+    (from..to).each do |day|
       menu = Menu.find_or_initialize_by(date: day)
       menu.update_attributes!({ cooking_repertoire_id: CookingRepertoire.random_id })
     end
