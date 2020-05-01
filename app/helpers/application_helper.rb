4318c904d5f675d52ac0578a9f9b3d3e623e240c
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-05-01 19:10:21 +0900

    menus_helperの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def periods
-    { I18n.t('.menus.new.one_day') => 1, I18n.t('.menus.new.seven_days') => 7 }
-  end
 end
---
+++ app/helpers/menus_helper.rb
@@ -0,0 +1,5 @@
+module MenusHelper
+  def periods
+    { I18n.t('.menus.new.one_day') => 1, I18n.t('.menus.new.seven_days') => 7 }
+  end
+end
