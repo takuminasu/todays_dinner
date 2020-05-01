aa070ba00206619ed5a7cc3ce974fd8d4893081e
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-05-01 15:40:35 +0900

    enum i18nの記述による不要な記述の削除,viewの記述の変更　transactionの追加

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,14 +1,17 @@
 class Menu < ApplicationRecord
-  enum period: { human_attribute_name('period.one_day') => 1, human_attribute_name('period.seven_days') => 7 }
+  enum period: { I18n.t('activerecord.attributes.menu.period.one_day') => 1,
+                 I18n.t('activerecord.attributes.menu.period.seven_days') => 7 }
   validates :date, presence: true, uniqueness: true
 
   belongs_to :cooking_repertoire
 
   def self.create_for(period, date)
     to = date + period - 1
-    (date..to).each do
-      menu = Menu.find_or_initialize_by(date: date)
-      menu.update_attributes({ date: date, cooking_repertoire_id: CookingRepertoire.random_id })
+    (date..to).each do |day|
+      transaction do
+        menu = Menu.find_or_initialize_by(date: day)
+        menu.update_attributes!({ date: day, cooking_repertoire_id: CookingRepertoire.random_id })
+      end
     end
   end
 end
