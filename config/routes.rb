31296a80d038280ce5f1636563924debf1a1808d
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-05-07 09:50:39 +0900

    不必要なルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cooking_repertoires, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :cooking_repertoires, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :tags, only: [:index]
-  resources :menus, only: [:index, :new, :create, :show]
+  resources :menus, only: [:index, :new, :create]
   root to: 'menus#index'
 end
