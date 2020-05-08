4eabe6b5775fd3a307c88155c527147116719cec
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-05-08 15:00:23 +0900

    レパートリー削除失敗時の処理を追加

--- app/controllers/cooking_repertoire_tags_controller.rb
+++ app/controllers/cooking_repertoire_tags_controller.rb
@@ -1,14 +1,17 @@
 class CookingRepertoireTagsController < ApplicationController
   def create
-    find_cooking_repertoire_tags
+    cooking_repertoire_and_tag
     cooking_repertoire_tag = CookingRepertoireTag.new(cooking_repertoire_id: @cooking_repertoire.id, tag_id: @tag.id)
-    cooking_repertoire_tag.save
-    redirect_to tags_path, notice: t('.deleted_repertoire', { name: @cooking_repertoire.name })
+    if cooking_repertoire_tag.save
+      redirect_to tags_path, notice: t('.deleted_repertoire', { name: @cooking_repertoire.name })
+    else
+      redirect_to cooking_repertoire_path(@cooking_repertoire), notice: t('.failed_delete')
+    end
   end
 
   private
 
-  def find_cooking_repertoire_tags
+  def cooking_repertoire_and_tag
     @cooking_repertoire = CookingRepertoire.find(params[:format])
     @tag = Tag.find_by(name: Tag.human_attribute_name(:delete))
   end
