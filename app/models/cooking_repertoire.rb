9b845e2976d6f482f3057686504251a24ac9289e
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-05-08 09:22:02 +0900

    レパートリー保存時にタグの個数にバリデーションをかける

--- app/models/cooking_repertoire.rb
+++ app/models/cooking_repertoire.rb
@@ -1,5 +1,6 @@
 class CookingRepertoire < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validate :cooking_repertoire_tags_record
 
   has_many :cooking_repertoire_tags
   has_many :tags, through: :cooking_repertoire_tags
@@ -7,4 +8,14 @@ class CookingRepertoire < ApplicationRecord
   has_many :menus
 
   scope :random_id, -> { offset(rand(CookingRepertoire.count)).first.id }
+
+  private
+
+  def cooking_repertoire_tags_record
+    if cooking_repertoire_tags.empty?
+      errors.add(:base, I18n.t('activerecord.errors.messages.no_select'))
+    elsif cooking_repertoire_tags.size > 3
+      errors.add(:base, I18n.t('activerecord.errors.messages.more_than_3'))
+    end
+  end
 end
