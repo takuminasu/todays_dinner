042249a65eeb5f0c2adc967ab0fd47ecb9965fa8
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-05-11 15:29:24 +0900

    seeds.rbとja.ymlファイルの消去を削除に変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ tags = Tag.create([{name: '和食'}, {name: '洋食'}, {name: '中華'}, {name: 
                    {name: '豚肉'}, {name: '牛肉'}, {name: '魚介'}, {name: '卵'},
                    {name: '大豆'}, {name: 'ご飯もの'}, {name: 'パスタ'}, {name: '甘い'},
                    {name: '辛い'}, {name: '苦い'}, {name: '酸っぱい'}, {name: '塩辛い'},
-                   {name: '春'}, {name: '夏'}, {name: '秋'}, {name: '冬'}, {name: '消去'}])
+                   {name: '春'}, {name: '夏'}, {name: '秋'}, {name: '冬'}, {name: '削除'}])
