f1113180fd7a364cf7dc5dee4431e65e9b5e678d
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-05-13 15:37:49 +0900

    controllerから呼ばれるものかを区別するためprivateメソッドを使用

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -11,23 +11,27 @@ class Menu < ApplicationRecord
     puts e.message
   end
 
-  def self.one_day(not_duplicate, day)
-    tags = exclude_tags(not_duplicate, day)
-    exclude_repertoire = CookingRepertoire.joins(:tags).where(tags: { id: tags })
-    cooking_repertoire_id = CookingRepertoire.valid.where.not(id: exclude_repertoire).sample.id
-    menu = Menu.find_or_initialize_by(date: day)
-    menu.update_attributes!({ cooking_repertoire_id: cooking_repertoire_id })
-  end
+  class << self
+    private
 
-  def self.exclude_tags(not_duplicate, day)
-    tags = []
-    (1..not_duplicate).each do |i|
-      before_day = day - i
-      before_menu = Menu.find_by(date: before_day)
-      cook_id = before_menu[:cooking_repertoire_id]
-      tag = CookingRepertoire.find(cook_id).tags
-      tags += tag
+    def one_day(not_duplicate, day)
+      tags = exclude_tags(not_duplicate, day)
+      exclude_repertoire = CookingRepertoire.joins(:tags).where(tags: { id: tags })
+      cooking_repertoire_id = CookingRepertoire.valid.where.not(id: exclude_repertoire).sample.id
+      menu = Menu.find_or_initialize_by(date: day)
+      menu.update_attributes!({ cooking_repertoire_id: cooking_repertoire_id })
     end
-    tags
+
+    def exclude_tags(not_duplicate, day)
+      tags = []
+      (1..not_duplicate).each do |i|
+        before_day = day - i
+        before_menu = Menu.find_by(date: before_day)
+        cook_id = before_menu[:cooking_repertoire_id]
+        tag = CookingRepertoire.find(cook_id).tags
+        tags += tag
+      end
+      tags
+    end
   end
 end
