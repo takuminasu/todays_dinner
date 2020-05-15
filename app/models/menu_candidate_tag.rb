a0a00d6f7c7e238d00466f38ecdb8113d768da49
Takumi Nasu <nasutaku7nasuka@gmail.com>
2020-05-15 14:46:22 +0900

    タグの候補を保存するレコードを差分だけ更新するようにする

--- app/models/menu_candidate_tag.rb
+++ app/models/menu_candidate_tag.rb
@@ -1,8 +1,9 @@
 class MenuCandidateTag < ApplicationRecord
   def self.make(tag_candidates)
-    MenuCandidateTag.destroy_all
+    MenuCandidateTag.where.not(tag_id: tag_candidates).destroy_all
     tag_candidates.each do |menu_candidate_tag|
-      MenuCandidateTag.create(tag_id: menu_candidate_tag.to_i)
+      tag_id = menu_candidate_tag.to_i
+      MenuCandidateTag.create(tag_id: tag_id) unless MenuCandidateTag.where(tag_id: tag_id).exists?
     end
   end
 end
